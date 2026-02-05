11ae1c06e948a1f3f654f86a61a19687cceb79f0
honeymodi55 <honeymodi1997@gmail.com>
2026-02-05 17:56:52 -0600

    deleting ec2 module

--- dev-terraform/modules/ec2_instance/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ami_value" {
-  description = "ami image for the ec2 instance"
-  type = string
-}
-
-variable "instance_type_value" {
-  description = "type of the ec2 instance"
-  type = string
-}
