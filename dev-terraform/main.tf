7496358babcd5bb49497bcd5f476ef0815eebb5b
honeymodi55 <honeymodi1997@gmail.com>
2026-02-05 17:34:08 -0600

    commented module ec2_instance in main.tf file

--- dev-terraform/main.tf
+++ dev-terraform/main.tf
@@ -19,11 +19,11 @@ provider "aws" {
 }
 
 
-/*module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-00a8151272c45cd8e"
-  instance_type_value = "t2.micro"
-}*/
+#module "ec2_instance" {
+#  source = "./modules/ec2_instance"
+#  ami_value = "ami-00a8151272c45cd8e"
+#  instance_type_value = "t2.micro"
+#}
 
 module "vpc" {
   source = "./modules/vpc"
