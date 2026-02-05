e835c1da7a0a22304bda2a9413df330bb523057e
honeymodi55 <honeymodi1997@gmail.com>
2026-02-05 17:51:45 -0600

    trigger change

--- dev-terraform/main.tf
+++ dev-terraform/main.tf
@@ -18,11 +18,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-00a8151272c45cd8e"
-  instance_type_value = "t2.micro"
-}
+#module "ec2_instance" {
+#  source = "./modules/ec2_instance"
+#  ami_value = "ami-00a8151272c45cd8e"
+#  instance_type_value = "t2.micro"
+#}
 
 module "vpc" {
   source = "./modules/vpc"
