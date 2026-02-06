8bbd35eb7b238b062462ef98c4f263cfef73f819
honeymodi55 <honeymodi1997@gmail.com>
2026-02-06 15:40:02 -0600

    creating EKS cluster and its managed node pools

--- dev-terraform/main.tf
+++ dev-terraform/main.tf
@@ -18,16 +18,46 @@ provider "aws" {
   region = "us-west-2"
 }
 
-#module "ec2_instance" {
-#  source = "./modules/ec2_instance"
-#  ami_value = "ami-00a8151272c45cd8e"
-#  instance_type_value = "t2.micro"
-#}
-
 module "vpc" {
   source = "./modules/vpc"
   vpc_cidr = "10.0.0.0/16"
   public_cidr = "10.0.1.0/24"
   private_cidr = "10.0.2.0/24"
   availability-zone = "us-west-2a"
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name = "apiApp-cluster"
+  kubernetes_version = "1.33"
+
+  endpoint_public_access = true
+  create_cloudwatch_log_group = true
+  cloudwatch_log_group_retention_in_days = 7
+
+
+  enable_cluster_creator_admin_permissions = true
+  authentication_mode = "API_AND_CONFIG_MAP"
+
+  vpc_id = module.vpc.vpc_id  
+  subnet_ids = module.vpc.private_subnet_id #for worker nodes
+  control_plane_subnet_ids = module.vpc.private_subnet_id #for control plane
+
+  #EKS managed node group
+  eks_managed_node_groups = {
+    api_node = {
+      ami_type = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+      min_size = 1
+      max_size = 2
+      desired_size = 1
+    }
+  }
+  tags = {
+    Name = "apiApp-cluster"
+    Environment = "dev"
+    Terraform = "true"
+  }
 }
